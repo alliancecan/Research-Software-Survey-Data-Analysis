1352d2bc8e0113e29786ea6b1e0e688ac815adbe
faresdhane <fs.dhane@gmail.com>
2023-06-05 16:37:11 -0400

    Question B7

--- Script/Script.R
+++ Script/Script.R
@@ -189,3 +189,141 @@ PieDonut(b2.domain.summary,
          pieLabelSize = 7)+
   scale_fill_manual(values =  cbp1)
 
+
+
+### B3 - Please choose the option that best describes your role at your institution. ######
+survey_B3_v1<- 
+  survey_organized_spread %>% 
+  select(Internal.ID, B3) %>% 
+  unnest(B3) %>% 
+  rename(Role = B3)
+
+sort(unique(survey_B3_v1$Role))# to clean the data
+
+survey_B3_v2 <- 
+  survey_B3_v1 %>% 
+  mutate(Role_n = ifelse(
+    Role == "Administratrice ou administrateur", "Administrator", ifelse(
+      Role == "Associee ou associe de recherche", "Research Associate", ifelse(
+        Role == "Autre", "Other", ifelse(
+          Role == "Bibliothecaire", "Librarian", ifelse(
+            Role == "Boursiere postdoctorale ou boursier postdoctoral", "Post-Doctoral Fellow", ifelse(
+              Role == "Chercheuse ou chercheur", "Researcher", ifelse(
+                Role == "Conseillere a la recherche", "Research Staff", ifelse(
+                  Role == "Developpeuse ou developpeur de logiciel de recherche", "Research Software Developer", ifelse(
+                    Role == "etudiante ou etudiant au doctorat", "Student (Doctoral)", ifelse(
+                      Role == "etudiante ou etudiant de premier cycle", "Student (Undergrad)", ifelse(
+                        Role == "etudiante ou etudiant diplome", "Student (Graduate)", ifelse(
+                          Role == "Ingenieure ou ingenieur en logiciels de recherche / Experte ou expert", "Research Software Engineer / Expert", ifelse(
+                            Role == "Personnel de recherche", "Research Staff", ifelse(
+                              Role == "Personnel de soutien", "Research Staff", ifelse(
+                                Role == "Professeure ou professeur auxiliaire, professeure ou professeur emerite, professeure ou professeur invite, professeure ou professeur avec mandat limite", "Faculty - Adjunct, emeritus, visiting, or limited-term", ifelse(
+                                  Role == "Professeure ou professeur, (incluant professeure ou professeur assistant, professeure ou professeur associe, professeure ou professeur titulaire, professeure ou professeur clinique, professeure ou professeur d_enseignement)", "Faculty - Professor (including assistant/associate/full professor, clinical professor, teaching professor)", ifelse(
+                                    Role == "staff in research support unit", "Research Staff", Role
+                                    ))))))))))))))))))
+
+
+##From "Other", avoid duplication and select answers
+
+#Add "2" to rows that I want to select
+duplications_v1 <- survey_B3_v2 %>% 
+  group_by(Internal.ID) %>% 
+  mutate(num_dups = n(), 
+         dup_id = row_number()) %>% 
+  ungroup() %>% 
+  mutate(is_duplicated = dup_id > 1)
+
+#select rows with dup_id = 2
+other_answe <- 
+  duplications_v1 %>% 
+  filter(dup_id == 2)
+
+#select rows with num_dups = 1
+duplications_v2 <- 
+  duplications_v1 %>% 
+  filter(num_dups == 1)
+
+#bind the two tables to have the final table
+survey_B3_v3 <- 
+  rbind(other_answe, duplications_v2) %>% 
+  select(Internal.ID, Role_n)
+
+#### summary table - Roles ####
+roles_summary <-
+  survey_B3_v3 %>%
+  group_by(Role_n) %>%
+  count() %>%
+  arrange(-n) %>%
+  drop_na() %>%
+  print()
+
+
+#### Pie charts ####
+PieDonut(roles_summary, 
+         aes(Role_n, count= n), 
+         ratioByGroup = FALSE, 
+         showPieName=FALSE, 
+         r0=0.25,r1=1,r2=1.4,start=pi/2,
+         labelpositionThreshold=1, 
+         showRatioThreshold = F, 
+         title= "Respondents' roles", 
+         titlesize = 5, pieAlpha = 1, donutAlpha = 1, color = "black")+ scale_fill_manual(values =  cbp_Cad) #+ 
+
+### B7 - Are you eligible to apply for and receive Tri-Council, CFI, or other research funding? ######
+survey_B7_v1<- 
+  survey_organized_spread %>% 
+  select(Internal.ID, B7) %>% 
+  unnest(B7)
+
+sort(unique(survey_B7_v1$B7))# to clean the data
+
+survey_B7_v2 <- 
+  survey_B7_v1 %>% 
+  mutate(B7_n = ifelse(
+    B7 == "Je ne sais pas", "Not sure", ifelse(
+      B7 == "Non", "No", ifelse(
+        B7 == "Oui", "Yes", B7
+        ))))
+
+B7_summay <- 
+  survey_B7_v2 %>% 
+  group_by(B7_n) %>% 
+  count()
+
+#### Pie chart #### 
+PieDonut(B7_summay, 
+         aes(B7_n, count= n), 
+         ratioByGroup = FALSE, 
+         showPieName=F, 
+         r0=0.0,r1=1,r2=1.4,start=pi/2,
+         labelpositionThreshold=1, 
+         showRatioThreshold = F, 
+         titlesize = 5, 
+         pieAlpha = 1, 
+         donutAlpha = 1, 
+         color = "black",
+         pieLabelSize = 7)+ 
+  scale_fill_manual(values =  cb_pie)
+
+
+### B8 - Quelles sont les sources de financement pour le budget réservé aux logiciels de recherche de votre groupe? ######
+survey_B8_v1<- 
+  survey_organized_spread %>% 
+  select(Internal.ID, B8) %>% 
+  unnest(B8)
+
+sort(unique(survey_B8_v1$B8))# to clean the data
+
+survey_B8_v2 <- 
+  survey_B8_v1 %>% 
+  mutate(B8_n = ifelse(
+    B8 == "Il n'est pas finance", "It is not funded", ifelse(
+      B8 == "Institutionnel", "Institutional", ifelse(
+        B8 == "Organisme international", "International funding", ifelse(
+          B8 == "Organisme provincial", "Provincial funding", ifelse(
+            B8 == "Subvention ou financement de l_industrie", "Industry grants", ifelse(
+              B8 == "IRSC", "CIHR", ifelse(
+                B8 == "CRSNG", "NSERC", ifelse(
+                  B8 == "CRSH", "SSHRC", ifelse(
+                    B8 == "FCI", "CFI", B8
+                    ))))))))))
